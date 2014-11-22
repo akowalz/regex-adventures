a82192ddf5427987631446715348040bf53f2044
Alex Kowalczuk <askowalczuk93@gmail.com>
2014-11-22 17:01:26 -0600

    regex highlighting

--- app/playCtrl.js
+++ app/playCtrl.js
@@ -3,14 +3,20 @@
 angular.module('myApp')
 .controller('playCtrl', function($scope) {
   $scope.regex = "";
+  $scope.matches = [];
   $scope.mtext = "This is some match text to test your regex on";
 
   $scope.runMatches = function () {
+    $("#match-text").highlightRegex();
+
     if ($scope.regex !== "" && validateRegex($scope.regex)) {
       var re = new RegExp($scope.regex, "g");
       $scope.matches = $scope.mtext.match(re);
+      $("#match-text").highlightRegex(re);
     } else if ($scope.regex == "") {
       $scope.matches = [];
+
+      $("#match-text").highlightRegex();
     }
   };
 
