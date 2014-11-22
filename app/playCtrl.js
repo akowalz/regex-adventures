9256193354643a2e4592333bedbb6a73fe907202
Alex Kowalczuk <askowalczuk93@gmail.com>
2014-11-22 16:10:34 -0600

    working as list

--- app/playCtrl.js
+++ app/playCtrl.js
@@ -1,7 +1,27 @@
 'use strict';
 
-angular.module('myApp.play', ['ngRoute'])
-
+angular.module('myApp')
 .controller('playCtrl', function($scope) {
-  $scope.matches = $scope.mtext.match($scope.regex);
+  $scope.regex = "";
+  $scope.mtext = "This is some match text to test your regex on";
+
+  $scope.runMatches = function () {
+    if ($scope.regex !== "" && validateRegex($scope.regex)) {
+      var re = new RegExp($scope.regex, "g");
+      $scope.matches = $scope.mtext.match(re);
+    } else if ($scope.regex == "") {
+      $scope.matches = [];
+    }
+  };
+
+  function validateRegex(regex) {
+    var valid;
+    try {
+      new RegExp(regex);
+      valid = true;
+    } catch (e) {
+      valid = false;
+    }
+    return valid;
+  }
 });
