550c26f118252cadac39edf718b492ee01c6ccba
Daniel Rasmuson <dan123911@gmail.com>
2014-11-23 02:29:11 -0600

    added a good version of notifications and a button

--- app/js/ctrl/Instructions.js
+++ app/js/ctrl/Instructions.js
@@ -1,10 +1,14 @@
 angular.module('app')
 .controller('InstructionsCtrl', function($scope) {
-    $scope.top = 0;
-    $scope.reflow = false;
-    $scope.body = 0;
-    $scope.hide = function(){
-        $scope.top = -400;
-        $scope.body = -160;
+    // $scope.top = 0;
+    // $scope.reflow = false;
+    // $scope.body = 0;
+    // $scope.hide = function(){
+    //     $scope.top = -400;
+    //     $scope.body = -160;
+    // }
+    $scope.instructionsOn = true;
+    $scope.toggleInstructions = function(){
+        $scope.instructionsOn = !$scope.instructionsOn; 
     }
 });
