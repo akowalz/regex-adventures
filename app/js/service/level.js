19c69363e609eae2e8950a1d3dccf7296499e49a
Daniel Rasmuson <dan123911@gmail.com>
2014-11-22 21:54:45 -0600

    added level service

---
+++ app/js/service/level.js
@@ -0,0 +1,29 @@
+angular.module('app')
+.service('LevelService', function() {
+    // array levels containing the answers
+    var answers = {
+        1:[],
+        2:[]
+    }
+
+    function isEqual(array1, array2){
+        for (var i = 0; i < array1.length; i++) {
+            if (array1[i] !== array2[i]){
+                return false;
+            }
+        };
+        return true;
+    }
+
+    // level starts at 0
+    var hasWon = function(level, currentAnswer){
+        if (answers[level].length !== currentAnswer.length){
+            return false;
+        }
+        return isEqual(answers[level].sort(), currentAnswer.sort());
+    };
+
+    return {
+        hasWon: hasWon
+    }
+});
